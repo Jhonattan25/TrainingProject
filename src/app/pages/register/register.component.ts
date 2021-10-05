2d97b42addb51b5c4122ca64210531275b26439f
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-10-05 01:04:26 -0500

    Variables were modified to register the user

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { LoginComponent } from '../../components/login/login.component'
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -14,17 +14,18 @@ import {MatDialog} from '@angular/material/dialog';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  cities!: Array<any>;
   //grupo de controles de nuestro formulario
   form!: FormGroup;
 
-  cities: Array<any> = [
+  /* cities: Array<any> = [
     {value: '1', viewValue: 'Armenia'},
     {value: '2', viewValue: 'Montenegro'},
     {value: '3', viewValue: 'Calarca'}
-  ];
+  ]; */
 
   //inyeccion de dependencias
-  constructor(public client:ClientService, private fb:FormBuilder, private router:Router, public dialog: MatDialog) { }
+  constructor(private client: ClientService, private fb: FormBuilder, private router: Router, public dialog: MatDialog) { }
 
   //en ngOnInit() metemos todas las instrucciones que queremos que se ejecuten apenas se cree nuestro componente
   ngOnInit(): void {
@@ -32,10 +33,12 @@ export class RegisterComponent implements OnInit {
     //estos controles se encuentran en cada input del formulario formControlName="cedula" y formControlName="password" 
     //se configuran los valores iniciales de cada input y las validaciones correspondientes
     this.form = this.fb.group({
-      cedula: ['', Validators.required],
-      nombre: ['', Validators.required],
-      email: ['', Validators.email],
-      password: ['', Validators.required]
+      identificationNumber: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(10)]],
+      fullName: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]],
+      cityCode: [''],
+      //cityCode: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(6)]],
     });
   }
 
@@ -46,28 +49,28 @@ export class RegisterComponent implements OnInit {
     if (this.form.valid) {
       //se envian los datos del formulario mediante una solicitud POST, los valores de los inputs del formulario 
       //se recogen usando los controles "email" y "password" para formar el json a enviar..
-      this.client.postRequestSendForm('http://localhost:10101/register', {
-        cedula: this.form.value.cedula,
-        nombre: this.form.value.nombre,
+      this.client.postRequestSendForm('http://localhost:10101/registerUser', {
+        identificationNumber: this.form.value.identificationNumber,
+        fullName: this.form.value.fullName,
         email: this.form.value.email,
         password: this.form.value.password,
-        idCiudad: 1
+        cityCode: "63001"
       }).subscribe(
         //cuando la respuesta del server llega es emitida por el observable mediante next()..
         (response: any) => {
           //se imprime la respuesta del server
           console.log(response);
-         
-          this.router.navigate( ['/']);
+
+          this.router.navigate(['/']);
           const dialogRef = this.dialog.open(LoginComponent);
-      },
-      //si ocurre un error en el proceso de envío del formulario...
-      (error) => {
-        //se imprime el status del error
-        console.log(error.status);
-      })
-    //si ocurrio un error en la validacion del formulario este no se enviara
-    //y se imprimira el mensaje "Form error"
+        },
+        //si ocurre un error en el proceso de envío del formulario...
+        (error) => {
+          //se imprime el status del error
+          console.log(error.status);
+        })
+      //si ocurrio un error en la validacion del formulario este no se enviara
+      //y se imprimira el mensaje "Form error"
     } else {
       console.log("Form error");
     }
