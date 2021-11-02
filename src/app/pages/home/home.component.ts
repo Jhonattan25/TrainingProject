d997d21578e38ba070cb9ce7d36d307b595428f2
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-11-02 00:19:54 -0500

    Validations were added for the home input and the template of the letter in which the searched documents will appear

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  documentNumber = new FormControl('',[Validators.required, Validators.minLength(8), Validators.maxLength(10)]);
+  showWarning:boolean = false;
+  showDocuments:boolean = true;
+
   constructor() { }
   ngOnInit(): void {
   }
-
-  showWarning:boolean = false;
   value = 'Clear me';
+  mail:string = "hola@gmail.com"
+
+  consultDocument(category:number){
+    console.log(this.documentNumber.dirty);
+
+    if(!this.documentNumber.errors){
+      this.showWarning = true;
+      setTimeout(()=>{
+        this.showWarning = false;
+      }, 2000);
+    }else{
+      this.showWarning = true;
+      setTimeout(()=>{
+        this.showWarning = false;
+      }, 2000);
+    }
+    
+  }
 }
