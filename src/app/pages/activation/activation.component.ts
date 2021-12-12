9c5ad0dd33002aaa9eeedd4efc6aaa06956d59bf
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-12-12 15:01:08 -0500

    Styles added to activation page

--- src/app/pages/activation/activation.component.ts
+++ src/app/pages/activation/activation.component.ts
@@ -9,25 +9,25 @@ import { ClientService } from '../../client.service';
 })
 export class ActivationComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute,private client:ClientService) { }
+  constructor(private activatedRoute: ActivatedRoute, private client: ClientService) { }
 
   ngOnInit(): void {
     this.activation();
 
-   // console.log(this.activatedRoute.snapshot.queryParams.id);
+    // console.log(this.activatedRoute.snapshot.queryParams.id);
   }
 
-  activation(){
+  activation() {
     this.activatedRoute.snapshot.queryParams.id
     this.client.updateRequestActivation(`http://localhost:10101/activation/?identificationNumber=${this.activatedRoute.snapshot.queryParams.identificationNumber}`).subscribe(
       //cuando la respuesta del server llega es emitida por el observable mediante next()..
       (response: any) => {
         console.log(response);
-    },
-    //si ocurre un error en el proceso de envío del formulario...
-    (error) => {
-      //se imprime el status del error
-      console.log(error.status);
+      },
+      //si ocurre un error en el proceso de envío del formulario...
+      (error) => {
+        //se imprime el status del error
+        console.log(error.status);
       }
     )
   }
